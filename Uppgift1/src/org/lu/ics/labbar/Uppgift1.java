1ee5f626aaf528b3a81c30e09c5e8847a5655c36
Michele von Huth <m.j.s.vonhuth@se18.qmul.ac.uk>
2019-10-15 17:24:04 +0200

    Update Uppgift1.java
    Added Comments

--- Uppgift1/src/org/lu/ics/labbar/Uppgift1.java
+++ Uppgift1/src/org/lu/ics/labbar/Uppgift1.java
@@ -10,12 +10,21 @@ public class Uppgift1 {
 		System.out.println("Skriv ett ord"); 
 		String ord1 = scanner1.nextLine(); 
 		
+		//get input from user and store in variable 'ord1' 
+		
 		Scanner scanner2 = new Scanner(System.in); 
 		System.out.println("Skriv ett annat ord"); 
 		String ord2 = scanner2.nextLine(); 
 		
+		//get second input from user and store in variable 'ord2' 
+		
 		String totalLength = ord1 + ord2; 
+		
+		//add a string of both inputs and store in variable 'totalLength'
+		
 		int length = totalLength.length(); 
+		
+		//use method .length() to count the amount of characters in variable 'totalLength' and store in an int variable
 		
 
 		System.out.println(ord1 + " " + ord2); 
